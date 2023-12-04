dfad2cb755d6d92174d522be94e960c088594421
Marijana Ravnjak <ravnjma@gmail.com>
2023-12-04 07:32:27 +0100

    Deploy websites

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
 
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename='post'>
                 <App />
             </BrowserRouter>
         </Provider>
